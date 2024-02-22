1f1f91e1fd72d94d5988ea261a175dd8218fbf19
Varun Singla <varun.singla@siemens.com>
2024-02-23 01:00:44 +0530

    Created a portal to show modal dialog at root level

--- RefsAndPortals/src/components/ResultModal.jsx
+++ RefsAndPortals/src/components/ResultModal.jsx
@@ -1,8 +1,10 @@
 import { forwardRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal({result, targetTime, remainingTime, onReset}, ref){
     const dialog = useRef();
     const userLost = remainingTime <= 0;
+    const score = Math.round((1-remainingTime/(targetTime*1000))*100)
     useImperativeHandle(ref, ()=>{
         return {
             open(){
@@ -10,14 +12,17 @@ const ResultModal = forwardRef(function ResultModal({result, targetTime, remaini
             }
         };
     })
-        return <dialog ref={dialog} className="result-modal">
+        return createPortal(<dialog ref={dialog} className="result-modal">
             {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>your score {score}</h2>}
             <p>The target time was <strong>{targetTime}</strong></p>
             <p>You stopped the time with <strong>{(remainingTime/1000).toFixed(2)} seconds left</strong></p>
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>, 
+        document.getElementById('modal')
+        )
     }
 )
 
